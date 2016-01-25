366c2eb7520c2167f6519c1ad2d88da38d3c1149
spencercree <mccullough.spencer@gmail.com>
2016-01-25 15:34:38 -0500

    added email catch

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,4 +7,14 @@ $(document).ready(function() {
     var idVal = this.id;
     $("#dimmer_"+idVal).removeClass("hover");
   });
+
+  $("#mc-embedded-subscribe").click(function() {
+    var clearInputs = function() {
+      $("#mce-EMAIL").val("");
+      $("#mce-FNAME").val("");
+    };
+    setTimeout(clearInputs, 2000);
+  });
+
+
 });
